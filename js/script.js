d6e00a0fb0d8b0437f242a0bf824271f0be154e5
Saf Venture <josafebalilI@gmail.com>
2021-10-20 16:37:20 +0800

    remove try

--- js/script.js
+++ js/script.js
@@ -252,31 +252,31 @@ window.onload = function() {
 }
 
 
-if(!window.parentPage)
-{
-    window.location.href = "https://kastilacientouno.vercel.app"
-}
+// if(!window.parentPage)
+// {
+//     window.location.href = "https://kastilacientouno.vercel.app"
+// }
 
-// document.addEventListener('contextmenu', function(e) {
-//   e.preventDefault();
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
 
   
-// });
+});
 
-// document.onkeydown = function(e) {
-//   if(event.keyCode == 123) {
-//      return false;
-//   }
-//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-//      return false;
-//   }
-//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-//      return false;
-//   }
-//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-//      return false;
-//   }
-//   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-//      return false;
-//   }
-// }
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}

b11b5ea1bba7364f91e95c8e5cc184289c354d21
Saf Venture <josafebalilI@gmail.com>
2021-10-31 13:40:53 +0800

    more

--- js/script.js
+++ js/script.js
@@ -469,26 +469,26 @@ window.onload = function() {
 
 }
 
-// document.addEventListener('contextmenu', function(e) {
-//   e.preventDefault();
+document.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
 
   
-// });
+});
 
-// document.onkeydown = function(e) {
-//   if(event.keyCode == 123) {
-//      return false;
-//   }
-//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-//      return false;
-//   }
-//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-//      return false;
-//   }
-//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-//      return false;
-//   }
-//   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-//      return false;
-//   }
-// }
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
